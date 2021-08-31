61a22045f7020ba018c1dd17c225cdb5e1501911
Benjamin <bmunyoki@heptanalytics.com>
2021-08-31 11:44:47 +0300

    Three column footer

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -73,33 +73,71 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-5 col-sm-6">
-                        <h2 style="margin-top: 8px;">CivicTech.Guide</h2>
-                        <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries.
-                        </p>
+                        <h2 style="margin-top: 8px;">Get email updates</h2>
+                        <!-- Begin MailChimp Signup Form -->
+                        <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+                        <div id="mc_embed_signup">
+                            <form action="https://guide.us19.list-manage.com/subscribe/post?u=cdb853c4847acff0d0a09460d&amp;id=ca5a70438b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                                <div id="mc_embed_signup_scroll">
+                                    <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+                                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_cdb853c4847acff0d0a09460d_ca5a70438b" tabindex="-1" value=""></div>
+                                    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                                </div>
+                            </form>
+                        </div>
+                        <!--End mc_embed_signup-->
+
                     </div>
                     <div class="col-md-4 col-sm-6 ">
-                        <h4>Helpful Links</h4>
-                        <ul class="footer-links">
-                            <li><a href="#">The Tech</a></li>
-                            <li><a href="#">The People</a></li>
+                        <h2>Helpful Links</h2>
+                        <ul class="footer-links" style="width: 100%;">
+                            <li><a href="https://civictech.guide/map/">Civic Tech World Map</a></li>
+                            <li><a href="https://civictech.guide/causes/">Projects by cause</a></li>
                         </ul>
-                        <ul class="footer-links">
-                            <li><a href="#">About</a></li>
+                    
+                        <ul class="footer-links" style="width: 100%;">
+                            <li><a href="https://civictech.guide/calendar/">Events</a></li>
+                            <li><a href="https://civictech.guide/research/">Research</a></li>
+                        </ul>
+                    
+                        <ul class="footer-links" style="width: 100%;">
+                            <li><a href="https://civictech.guide/what-can-i-do-with-the-civic-tech-field-guide/">How to use this site</a></li>
+                            <li><a href="https://civictech.guide/guiding-principles/">Guiding principles</a></li>
+                            <li><a href="https://civictech.guide/blog/">Blog</a></li>
                         </ul>
                         <div class="clearfix"></div>
                     </div>
                     <div class="col-md-3  col-sm-12">
-                        <h4>Reach Out</h4>
+                        <h2>Reach Out</h2>
                         <div class="text-widget">
-                            <span>12345 NYC</span> <br>
-                            Phone: <span>123-456 </span><br>
-                            E-Mail:<span> <a href="#">info@email.com</span></a> </span><br>
+                            <span>
+                                info@civictech.guide or <a href="https://civictech.guide/contact">Contact us</a>
+                            </span>
                         </div>
                         <ul class="social-icons margin-top-20">
-                            <li><a class="facebook" href="#"><i class="icon-facebook"></i></a></li>
-                            <li><a class="twitter" href="#"><i class="icon-twitter"></i></a></li>
+                            <li>
+                                <a class="facebook" href="https://www.facebook.com/civictechguide/"><i class="icon-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a class="twitter" href="https://twitter.com/civictechguide"><i class="icon-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a class="instagram" href="https://www.instagram.com/civictechguide/"><i class="icon-instagram"></i></a>
+                            </li>
                         </ul>
+                        <div class="clearfix">&nbsp;&nbsp;</div>
+                        <div class="text-widget">
+                            <h4>Founding organization</h4>
+                            <img src="{{ asset('images/civic_hall.png') }}" />
+                        </div>
+                        <div class="clearfix">&nbsp;&nbsp;</div>
+                        <div class="text-widget">
+                            <h4>With support from</h4>
+                            <img src="{{ asset('images/knight_foundation.png') }}" />
+                        </div>
+                        <div class="clearfix">&nbsp;&nbsp;</div>
+                        
                     </div>
                 </div>
                 <!-- Copyright -->
