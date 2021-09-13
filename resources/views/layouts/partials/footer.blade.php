5238dcb134cbbafb6cf2f2ed73a50638e50580fc
Benjamin <bmunyoki@heptanalytics.com>
2021-09-13 21:01:31 +0300

    Project map marker to link to listing profile, aspect ratio for footer images

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,97 @@
+<div id="footer" class="margin-top-15">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 col-sm-6">
+                <h2 style="margin-top: 8px;">Get email updates</h2>
+                <!-- Begin MailChimp Signup Form -->
+                <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+                <div id="mc_embed_signup">
+                    <form action="https://guide.us19.list-manage.com/subscribe/post?u=cdb853c4847acff0d0a09460d&amp;id=ca5a70438b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                        <div id="mc_embed_signup_scroll">
+                            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+                            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_cdb853c4847acff0d0a09460d_ca5a70438b" tabindex="-1" value=""></div>
+                            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                        </div>
+                    </form>
+                </div>
+                <!--End mc_embed_signup-->
+
+            </div>
+            <div class="col-md-4 col-sm-6 ">
+                <h2>Helpful Links</h2>
+                <ul class="footer-links" style="width: 100%;">
+                    <li><a href="https://civictech.guide/map/">Civic Tech World Map</a></li>
+                    <li><a href="https://civictech.guide/causes/">Projects by cause</a></li>
+                </ul>
+            
+                <ul class="footer-links" style="width: 100%;">
+                    <li><a href="https://civictech.guide/calendar/">Events</a></li>
+                    <li><a href="https://civictech.guide/research/">Research</a></li>
+                </ul>
+            
+                <ul class="footer-links" style="width: 100%;">
+                    <li><a href="https://civictech.guide/what-can-i-do-with-the-civic-tech-field-guide/">How to use this site</a></li>
+                    <li><a href="https://civictech.guide/guiding-principles/">Guiding principles</a></li>
+                    <li><a href="https://civictech.guide/blog/">Blog</a></li>
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+            <div class="col-md-3  col-sm-12">
+                <h2>Reach Out</h2>
+                <div class="text-widget">
+                    <span>
+                        info@civictech.guide or <a href="https://civictech.guide/contact">Contact us</a>
+                    </span>
+                </div>
+                <ul class="social-icons margin-top-20">
+                    <li>
+                        <a class="facebook" href="https://www.facebook.com/civictechguide/"><i class="icon-facebook"></i></a>
+                    </li>
+                    <li>
+                        <a class="twitter" href="https://twitter.com/civictechguide"><i class="icon-twitter"></i></a>
+                    </li>
+                    <li>
+                        <a class="instagram" href="https://www.instagram.com/civictechguide/"><i class="icon-instagram"></i></a>
+                    </li>
+                </ul>
+                <div class="clearfix">&nbsp;&nbsp;</div>
+            </div>
+        </div>
+    </div>
+    <div class="container" style="width: 100%;">
+        <div class="row">
+            <div class="col-md-12" style="text-align: center;">
+                <div class="row social-icons margin-top-20">
+                    <div class="col-md-3">
+                        <h3>Founding organization</h3>
+                        <img width="150" height="150" src="{{ asset('images/civic_hall.png') }}" />
+                    </div>
+                    <div class="col-md-6">
+                        <h3>With support from</h3>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <img src="{{ asset('images/knight_foundation.png') }}" />
+                            </div>
+                            <div class="col-md-6" style="vertical-align: text-top;">
+                                <img src="{{ asset('images/luminate.png') }}" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <h3>Technology Partner</h3>
+                        <img src="{{ asset('images/sarapis.png') }}" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <!-- Copyright -->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="copyrights"><img width="18" height="18" src="{{ asset('images/cc.png') }}" /> Civic Tech Field Guide</div>
+            </div>
+        </div>
+    </div>
+</div>
