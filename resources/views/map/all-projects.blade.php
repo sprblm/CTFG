762bd541bacb2682a412dc376e6230e858c61d64
Benjamin <bmunyoki@heptanalytics.com>
2021-09-14 12:02:09 +0300

    Cluster spreadfier for many projects in the same address

--- resources/views/map/all-projects.blade.php
+++ resources/views/map/all-projects.blade.php
@@ -27,81 +27,79 @@
         var projects = {!! json_encode($projects->toArray(), JSON_HEX_TAG) !!};
 
         function initMap() {
-          // map options
-          var options = {
-            zoom: 2,
-            center: new google.maps.LatLng(0, 0),
-            streetViewControl: false,
-            mapTypeControl: false,
-          }
+            // map options
+            var options = {
+                zoom: 2,
+                center: new google.maps.LatLng(0, 0),
+                streetViewControl: false,
+                mapTypeControl: false,
+            }
 
-          var map = new google.maps.Map(document.getElementById('worldMap'), options);
+            var map = new google.maps.Map(document.getElementById('worldMap'), options);
 
-          var oms = new OverlappingMarkerSpiderfier(map, {
-            markersWontMove: true,
-            markersWontHide: true,
-            basicFormatEvents: true,
-            ignoreMapClick: true,
-            keepSpiderfied: true
-          });
+            var oms = new OverlappingMarkerSpiderfier(map, {
+                markersWontMove: true,
+                markersWontHide: true,
+                basicFormatEvents: true,
+                ignoreMapClick: true,
+                keepSpiderfied: true
+            });
 
-            var markers = [
-            ];
+            var markers = [];
 
             for (var i = 0; i < projects.length; i++) {
                 var latitude = Number(projects[i].latitude);
                 var longitude = Number(projects[i].longitude);
                 var project = projects[i].name;
-                var add = projects[i].first_location;
-                var slug = '/listing/'+projects[i].slug;
+                var address = projects[i].first_location;
+                var url = '/listing/'+projects[i].slug;
 
                 var entry = {
                     coords: {lat: latitude, lng: longitude},
                     content: '<div id="content">' +
-                    '<div id="siteNotice">' +
-                    "</div>" +
-                    '<h3 id="firstHeading" class="firstHeading"><a class="location" title="Go to listing profile" href="'+slug+'">'+project+'</a></h3>' +
-                    '<div id="bodyContent">' +
-                    "<p><i class='fa fa-map-marker'></i> "+add+"</p>" +
-                    "</div>" +
-                    "</div>"
+                        '<div id="siteNotice">' +
+                        "</div>" +
+                        '<h3 id="firstHeading" class="firstHeading"><a class="location" title="Go to listing profile" href="'+url+'">'+project+'</a></h3>' +
+                        '<div id="bodyContent">' +
+                        "<p><i class='fa fa-map-marker'></i> "+address+"</p>" +
+                        "</div>" +
+                        "</div>"
                 }
 
                 markers.push(entry);
             }
 
-          // Loop through markers
-          var gmarkers = [];
-          for(var i = 0; i < markers.length; i++){
-            gmarkers.push(addMarker(markers[i]));
-          }
-
-          //Add MArker function
-          function addMarker(props){
-            var marker = new google.maps.Marker({
-              position:props.coords,
-              map:map
-            });
-
-            //Check content
-            if(props.content){
-              var infoWindow = new google.maps.InfoWindow({
-                content:props.content
-              });
-              marker.addListener('click',function(){
-                infoWindow.open(map,marker);
-              });
+            // Loop through markers
+            var gmarkers = [];
+            for(var i = 0; i < markers.length; i++){
+                gmarkers.push(addMarker(markers[i]));
             }
+
+            //Add MArker function
+            function addMarker(props){
+                var marker = new google.maps.Marker({
+                    position:props.coords,
+                    map:map
+                });
+
+                // Check content
+                if(props.content){
+                    var infoWindow = new google.maps.InfoWindow({
+                        content:props.content
+                    });
+                    marker.addListener('click',function(){
+                        infoWindow.open(map,marker);
+                    });
+                }
                 
-            oms.trackMarker(marker);
+                oms.trackMarker(marker);
                 
-            return marker;
-          }
+                return marker;
+            }
           
-          var markerCluster = new MarkerClusterer(map, gmarkers, 
-            {
-              maxZoom: 19,
-              imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+            var markerCluster = new MarkerClusterer(map, gmarkers, {
+                maxZoom: 19,
+                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
             });
         }
           
