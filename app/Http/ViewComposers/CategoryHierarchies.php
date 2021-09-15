351b4b502f74f3825b38dd57541e4b1662c925fc
Benjamin <bmunyoki@heptanalytics.com>
2021-09-15 14:43:30 +0300

    Database caching

--- app/Http/ViewComposers/CategoryHierarchies.php
+++ app/Http/ViewComposers/CategoryHierarchies.php
@@ -4,6 +4,8 @@ namespace App\Http\ViewComposers;
 
 use Illuminate\View\View;
 
+use Cache;
+
 use App\Models\Category;
 use App\Models\Tag;
 
@@ -17,9 +19,17 @@ class CategoryHierarchies {
      * @return void
      */
     public function __construct() {
-        $catHierarchies = Category::whereNull('parent_id')->whereIn('name', ['The Tech', 'The People', 'Adjacent Fields', 'More'])->orderByRaw("FIELD(name , 'The Tech', 'The People', 'Adjacent Fields', 'More') ASC")->with('childItems')->get();
+        $catHierarchies = Cache::remember('category-hierarchies', 43200, function () {
+            return Category::whereNull('parent_id')->whereIn('name', ['The Tech', 'The People', 'Adjacent Fields', 'More'])->orderByRaw("FIELD(name , 'The Tech', 'The People', 'Adjacent Fields', 'More') ASC")->with('childItems')->get();
+        });
 
-        $tagHierarchies = Tag::whereNull('parent_id')->with('childItems')->get();
+        $tagHierarchies = Cache::remember('tag-hierarchies', 43200, function () {
+            return Tag::whereNull('parent_id')->with('childItems')->get();
+        });
+
+        /*$catHierarchies = Category::whereNull('parent_id')->whereIn('name', ['The Tech', 'The People', 'Adjacent Fields', 'More'])->orderByRaw("FIELD(name , 'The Tech', 'The People', 'Adjacent Fields', 'More') ASC")->with('childItems')->get();
+
+        $tagHierarchies = Tag::whereNull('parent_id')->with('childItems')->get(); */
 
         $this->catHierarchies = $catHierarchies;
         $this->tagHierarchies = $tagHierarchies;
