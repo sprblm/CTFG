0079dbbc7ec9ec4f7cc4eb9b57ad3b5605e09cc6
Benjamin <bmunyoki@heptanalytics.com>
2021-10-04 20:18:52 +0300

    Changes to Google Analytics code

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -7,16 +7,15 @@
     <link rel="stylesheet" href="{{ asset('css/listing.css') }}">
     <link rel="stylesheet" href="{{ asset('css/main-color.css') }}" id="colors">
     @yield('styles')
-    <!-- Google Analytics -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-R1HZWFGK53"></script>
     <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
 
-        ga('create', 'G-R1HZWFGK53', 'auto');
-        ga('send', 'pageview');
+      gtag('config', 'G-R1HZWFGK53');
     </script>
-    <!-- End Google Analytics -->
 </head>
 <body>
     <!-- Wrapper -->
