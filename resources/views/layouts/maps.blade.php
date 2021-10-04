5c96f8b584f7faf2a09954455fd431e758b25b5b
Benjamin <bmunyoki@heptanalytics.com>
2021-10-04 19:51:37 +0300

    Changes to Google Analytics code

--- resources/views/layouts/maps.blade.php
+++ resources/views/layouts/maps.blade.php
@@ -13,7 +13,7 @@
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'G-T3J4EWQG5R', 'auto');
+        ga('create', 'G-R1HZWFGK53', 'auto');
         ga('send', 'pageview');
     </script>
     <!-- End Google Analytics -->
