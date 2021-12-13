8cf64801dae71297f43f3c9c653311660a5402c6
Benjamin <bmunyoki@heptanalytics.com>
2021-12-13 16:18:20 +0300

    Bug fix - enforce https on pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot() {
         if($this->app->environment('production')) {
             \URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS','on');
         }
         /*DB::listen(function($query) {
             File::append(
