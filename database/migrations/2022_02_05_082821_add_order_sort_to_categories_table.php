a2216bce437550edebb94fc80f4106e55349ba64
Benjamin <bmunyoki@heptanalytics.com>
2022-02-05 08:32:01 +0300

    Added order sort column to categories

---
+++ database/migrations/2022_02_05_082821_add_order_sort_to_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOrderSortToCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->smallInteger('order_sort')->nullable()->after('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            //
+        });
+    }
+}
