9f6c43be074ca412aa6dc7e22ae2a5efab31dfad
Mészáros Róbert <hmm@meszarosrob.com>
2022-04-13 18:35:58 +0300

    Add visually hidden labels for newsletter inputs

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -8,9 +8,13 @@
                 <div id="mc_embed_signup">
                     <form action="https://guide.us19.list-manage.com/subscribe/post?u=cdb853c4847acff0d0a09460d&amp;id=ca5a70438b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                         <div id="mc_embed_signup_scroll">
+                            <label for="mce-EMAIL" class="visually-hidden">Email address</label>
                             <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
                             <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_cdb853c4847acff0d0a09460d_ca5a70438b" tabindex="-1" value=""></div>
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                                <label for="mce-honeypont" class="visually-hidden">Age (honeypot)</label>
+                                <input id="mce-honeypont" type="text" name="b_cdb853c4847acff0d0a09460d_ca5a70438b" tabindex="-1" value="">
+                            </div>
                             <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
                         </div>
                     </form>
