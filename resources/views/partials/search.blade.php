28f93a2409d2f4d71b72e9e4e4f24a9e15bc16dd
Mészáros Róbert <hmm@meszarosrob.com>
2022-04-13 17:39:18 +0300

    Add visually hidden label for search input

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,8 +1,9 @@
 <div class="row">
     <div class="col-md-10">
         <div class="main-search-input gray-style margin-top-0 margin-bottom-10" style="z-index: 10000;">
+            <label for="project-search-input" class="visually-hidden">Search Civic Tech Field Guide projects</label>
             <div class="main-search-input-item">
-                <input type="text" class="typeahead tt-query" placeholder="Search Civic Tech Field Guide projects ... " autocomplete="off" value="{{ @$query }}" />
+                <input id="project-search-input" type="text" class="typeahead tt-query" placeholder="Search Civic Tech Field Guide projects ... " autocomplete="off" value="{{ @$query }}" />
             </div>
 
             <button class="button" id="search">Search</button>
