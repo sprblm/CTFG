526854d9c910a3551ea0b2c4b16aaa451b325f12
Benjamin <bmunyoki@heptanalytics.com>
2022-06-25 00:26:27 +0300

    Schema for boundaries table, sync boundaries table, link locations to boundaries

--- app/Http/Controllers/Api/TP/DocuController.php
+++ app/Http/Controllers/Api/TP/DocuController.php
@@ -25,4 +25,25 @@ class DocuController extends Controller {
             'error' => 'Confs not found'
         ], 404);
     }
+
+    public function updateConf(Request $request) {
+        $name = $request->segment(4);
+
+        $confs = DocuConfs::where('name', $name)->first();
+
+        if ($confs) {
+            $confs->update([
+                'access' => $request->input('access'),
+                'refresh' => $request->input('refresh'),
+            ]);
+
+            return response()->json([
+                'message' => 'Success',
+            ], 200);
+        }
+
+        return response()->json([
+            'error' => 'Confs not found'
+        ], 404);
+    }
 }
