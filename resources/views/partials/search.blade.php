b529bba43724f99a5c1ddf9cda6d2b4215b41be6
Benjamin <bmunyoki@heptanalytics.com>
2022-07-15 12:46:19 +0300

    Bug fix - advanced filters scroll

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -22,7 +22,7 @@
             <div class="main-search-box no-shadow margin-bottom-30" style="border-bottom: 2px dotted #ccc;">
                 <div class="row with-forms margin-bottom-30">
                     <!-- <input type="hidden" name="q" value="{{ @$query }}"> -->
-                    <div class="col-md-4">
+                    <div class="col-md-4 col-sm-12">
                         <label for="filter-categories-input" class="visually-hidden">Categories</label>
                         <select id="filter-categories-input" name="categories[]" data-placeholder="All Categories" class="chosen-select-no-single" multiple style="display: none;">
                             @foreach($categories as $cat)
@@ -37,7 +37,7 @@
                         </select>
                     </div>
 
-                    <div class="col-md-4">
+                    <div class="col-md-4 col-sm-12">
                         <label for="filter-tags-input" class="visually-hidden">Tags</label>
                         <select id="filter-tags-input" name="tags[]" data-placeholder="All Tags" class="chosen-select-no-single" multiple style="display: none;">
                             @foreach($allTags as $tag)
@@ -52,7 +52,7 @@
                         </select>
                     </div>
 
-                    <div class="col-md-4">
+                    <div class="col-md-4 col-sm-12">
                         <label for="filter-project-type-input" class="visually-hidden">Project Type</label>
                         <select id="filter-project-type-input" name="types[]" data-placeholder="Project type" class="chosen-select-no-single" multiple style="display: none;">
                             @foreach($listingTypes as $type)
@@ -67,7 +67,7 @@
                         </select>
                     </div>
 
-                    <div class="col-md-4">
+                    <div class="col-md-4 col-sm-12">
                         <label for="filter-organization-type-input" class="visually-hidden">Organization Type</label>
                         <select id="filter-organization-type-input" name="organizationtypes[]" data-placeholder="Organization type" class="chosen-select-no-single" multiple style="display: none;">
                             @foreach($organizationTypes as $orgType)
@@ -82,7 +82,7 @@
                         </select>
                     </div>
                     
-                    <div class="col-md-4">
+                    <div class="col-md-4 col-sm-12">
                         <label for="filter-countries-input" class="visually-hidden">Countries</label>
                         <select id="filter-countries-input" name="countries[]" data-placeholder="All Countries" id="countries" class="chosen-select-no-single" multiple style="display: none;">
                             @foreach($allCountries as $country)
@@ -97,7 +97,7 @@
                         </select>
                     </div>
 
-                    <div class="col-md-4">
+                    <div class="col-md-4 col-sm-12">
                         <label for="filter-open-source-input" class="visually-hidden">Open Source</label>
                         <select id="filter-open-source-input" name="opensource[]" data-placeholder="Open source" class="chosen-select-no-single" multiple style="display: none;">
                             <?php
