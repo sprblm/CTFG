15cc2f7543d9ff1353760368a20a98dc641125ad
Benjamin <bmunyoki@heptanalytics.com>
2022-07-19 11:21:18 +0300

    Creating escaped slugs from project names

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -103,11 +103,6 @@
     <script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.7/dist/loadingoverlay.min.js"></script>
     
     <script type="text/javascript">
-        /*$(window).load(function() {
-            $.LoadingOverlay("hide");
-        }); */
-        
-
         var path = "{{ route('autocomplete') }}";
 
         /*$('input.typeahead').typeahead({
