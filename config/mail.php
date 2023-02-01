34d741b9d77dd2e4cc32960e5fa5527c192f5889
bmunyoki <munyokibenjamin@gmail.com>
2023-02-01 10:47:54 +0300

    Listing contact form email

--- config/mail.php
+++ config/mail.php
@@ -84,8 +84,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'contact@civictech.guide'),
+        'name' => env('MAIL_FROM_NAME', 'Civic Tech Field Guide'),
     ],
 
     /*
