52aeb01fa0217c35c7c2343be9ebe1de0b3a4c75
bmunyoki <munyokibenjamin@gmail.com>
2023-06-06 12:46:36 +0300

    Countries filter bug fix

--- app/Http/Controllers/Category/ProjectController.php
+++ app/Http/Controllers/Category/ProjectController.php
@@ -80,7 +80,8 @@ class ProjectController extends Controller {
                     $builder->whereHas('location', function($builder) use ($countries) {
                         $builder->where( function($builder) use ($countries) {
                             foreach ($countries as $country) {
-                                $builder->orWhere('country', 'LIKE', '%' . $country . '%');
+                                //$builder->orWhere('country', 'LIKE', '%' . $country . '%');
+                                $builder->orWhere('name', 'LIKE', '%' . $country . '%');
                             }
                         });
                     });
