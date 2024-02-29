1c25336c48a0a3e74b566a878f055199740cd757
bmunyoki <munyokibenjamin@gmail.com>
2024-02-29 21:17:56 +0300

    Bug fix - some listing not rendering cover image

--- resources/views/partials/paginated-projects.blade.php
+++ resources/views/partials/paginated-projects.blade.php
@@ -9,7 +9,7 @@
     <div class="listing-item">
         <a href="/listing/{{ $project->slug }}" class="listing-img-container">
             @if(@$project->media->first())
-                <img src="{{ @$project->media->first()->link }}" loading="lazy" alt="{{ $project->name }}">
+                <img src="{{ @$project->media->first()->link }}" loading="lazy" alt="{{ $project->name }}" width="327px" height="204px">
             @else
                 <img src="{{ asset('images/gray.png') }}" loading="lazy" alt="{{ $project->name }}">
             @endif
