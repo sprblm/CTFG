193dd61340994f85d94bddd3ec40919e6cff6744
bmunyoki <munyokibenjamin@gmail.com>
2024-03-05 15:58:33 +0300

    Search active projects only to include null

--- app/Http/Controllers/Projects/ProjectController.php
+++ app/Http/Controllers/Projects/ProjectController.php
@@ -110,7 +110,7 @@ class ProjectController extends Controller {
             ->when(request('status'), function($builder) {
                 $status = request('status');
                 if ($status == "Show active projects only") {
-                    $builder->whereNull('status')->orWhereIn('status', ['Active', 'N/A']);
+                    $builder->whereIn('status', ['Active', 'N/A']);
                 } else {
                     //$builder->whereIn('status', ['Active', 'N/A', 'Inactive', 'Document'])->orWhereNull('status');
                     $builder = $builder;
